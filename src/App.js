9ca3f21a9965f52191aa8ab2e007febd21374f73
yogesh satkari <yogeshsatkari2@gmail.com>
2023-06-15 01:45:05 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import Axios from "axios"
+import { useState, useEffect} from "react"
+
 
 function App() {
+
+  const [UserProfiles, setUserProfiles] = useState([])
+  useEffect(() => {
+    async function getUserProfiles() {
+      const response = await Axios.get('http://localhost:3001/user-profile')
+      console.log('user-profile response : ', response)
+      setUserProfiles(response.data)
+    }
+    getUserProfiles();
+    console.log(UserProfiles)
+  }, [])
+  
+
   return (
     <div className="App">
       <header className="App-header">
